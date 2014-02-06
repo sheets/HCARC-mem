902b7df888236feea0b8d4477cea32848f6f3d15
vivek <vivek@vivek-laptop.(none)>
2014-02-07 00:00:30 +0530

    [my first commit]

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hcare::Application.config.secret_token = 'ae6af5284a4e83ce62323bc61409d21097437ccc9e258413bb8b2f66dcee9576adafac13e528dd6952b77cc83a3f6324afbaf068a072d88ae964569241c899ec'
