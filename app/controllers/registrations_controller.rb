db636f1c63aa224d5f7e2079bed8233686b4ded6
vivek <vivek@vivek-laptop.(none)>
2014-02-16 02:21:25 +0530

    [skip call sign]

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,30 +1,8 @@
 class RegistrationsController < Devise::RegistrationsController
+	before_filter :call_sign_call,:only=>[:new]
 	require 'nokogiri'
 	require 'open-uri'
 	def new
-		url="http://xmldata.qrz.com/xml/current/?s=#{cookies[:xml_key]}&callsign=kb4mbs"
-		xml = Nokogiri::XML(open(url))
-		xml_path=xml.xpath("//xmlns:Callsign")
-		if xml_path.blank?
-			create_session
-			url="http://xmldata.qrz.com/xml/current/?s=#{cookies[:xml_key]}&callsign=kb4mbs"
-			xml = Nokogiri::XML(open(url))
-			xml_path=xml.xpath("//xmlns:Callsign")
-		end
-		@alldata={}
-		xml_path.each do |node|
-		    @alldata["call"]=node.xpath("//xmlns:call").first.children.text
-		    @alldata["fname"]=node.xpath("//xmlns:fname").first.children.text
-		    @alldata["name"]=node.xpath("//xmlns:name").first.children.text
-		    @alldata["addr1"]=node.xpath("//xmlns:addr1").first.children.text
-		    @alldata["addr2"]=node.xpath("//xmlns:addr2").first.children.text
-		    @alldata["state"]=node.xpath("//xmlns:state").first.children.text
-		    @alldata["zip"]=node.xpath("//xmlns:zip").first.children.text
-		    @alldata["class_user"]=node.xpath("//xmlns:class").first.children.text
-		    @alldata["lat"]=node.xpath("//xmlns:lat").first.children.text
-		    @alldata["efdate"]=node.xpath("//xmlns:efdate").first.children.text
-		    @alldata["expdate"]=node.xpath("//xmlns:expdate").first.children.text
-		end
 		@user=User.new
 		@user.build_userprofile
 	end
@@ -43,10 +21,41 @@ class RegistrationsController < Devise::RegistrationsController
 		super
 	end
 	private
+	def call_sign_call
+		if params[:call_sign].present?
+			url="http://xmldata.qrz.com/xml/current/?s=#{cookies[:xml_key]}&callsign=kb4mbs"
+			xml = Nokogiri::XML(open(url))
+			xml_path=xml.xpath("//xmlns:Callsign")
+			if xml_path.blank?
+				create_session
+				url="http://xmldata.qrz.com/xml/current/?s=#{cookies[:xml_key]}&callsign=kb4mbs"
+				xml = Nokogiri::XML(open(url))
+				xml_path=xml.xpath("//xmlns:Callsign")
+			end
+			@alldata={}
+			xml_path.each do |node|
+			    @alldata["call"]=node.xpath("//xmlns:call").first.children.text
+			    @alldata["fname"]=node.xpath("//xmlns:fname").first.children.text
+			    @alldata["name"]=node.xpath("//xmlns:name").first.children.text
+			    @alldata["addr1"]=node.xpath("//xmlns:addr1").first.children.text
+			    @alldata["addr2"]=node.xpath("//xmlns:addr2").first.children.text
+			    @alldata["state"]=node.xpath("//xmlns:state").first.children.text
+			    @alldata["zip"]=node.xpath("//xmlns:zip").first.children.text
+			    @alldata["class_user"]=node.xpath("//xmlns:class").first.children.text
+			    @alldata["lat"]=node.xpath("//xmlns:lat").first.children.text
+			    @alldata["efdate"]=node.xpath("//xmlns:efdate").first.children.text
+			    @alldata["expdate"]=node.xpath("//xmlns:expdate").first.children.text
+			end
+		else
+			@alldata=""
+		end
+	end
 	def create_session
-		url_auth="http://xmldata.qrz.com/xml/current/?username=kb4mbs&password=scxcdr34"
-		xml_auth = Nokogiri::XML(open(url_auth))
-		cookies[:xml_key] = { :value => xml_auth.xpath("//xmlns:Session//xmlns:Key").first.children.text, :expires => Time.now + 3600}
+		if params[:call_sign].present?
+			url_auth="http://xmldata.qrz.com/xml/current/?username=#{params[:call_sign]}&password=scxcdr34"
+			xml_auth = Nokogiri::XML(open(url_auth))
+			cookies[:xml_key] = { :value => xml_auth.xpath("//xmlns:Session//xmlns:Key").first.children.text, :expires => Time.now + 3600}
+		end
 	end
 	
 end 
